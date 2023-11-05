0d800a0c3dfd91cc5116594843d70823130fcc83
hulele <hulelebuaa@163.com>
2023-11-05 13:21:27 +0800

    支持链路退出时通知所有等待本链路返回响应报文的链路不用等待，直接返回模型已经退出

--- server/model.go
+++ server/model.go
@@ -45,150 +45,149 @@ const proxyMetaJSON = `
 }
 `
 
-type ModelConnection struct {
-	net.Conn                                              // 原始连接
-	quit               chan struct{}                      // 退出writer的信号
-	localSubStateCh    chan updateSubTableMsg             // 更新本地状态订阅通道
-	localSubEventCh    chan updateSubTableMsg             // 更新本地事件订阅通道
-	remoteSubStateCh   chan updateSubTableMsg             // 更新远端状态订阅通道, 会触发订阅状态报文发送
-	remoteSubEventCh   chan updateSubTableMsg             // 更新远端事件订阅通道, 会触发订阅事件报文发送
-	querySubState      chan chan []string                 // 查询状态订阅通道
-	querySubEvent      chan chan []string                 // 查询事件订阅通道
-	removeConnCh       chan<- *ModelConnection            // 删除连接通道
-	stateBroadcast     chan<- message.StateOrEventMessage // 状态广播通道
-	eventBroadcast     chan<- message.StateOrEventMessage // 事件广播通道
-	callChan           chan<- message.CallMessage         // 调用请求通道
-	respChan           chan<- message.ResponseMessage     // 响应结果通道
-	serverDone         <-chan struct{}                    // Server 完成退出信息
-	stateWriteChan     chan message.StateOrEventMessage   // 状态写入通道
-	eventWriteChan     chan message.StateOrEventMessage   // 事件写入通道
-	callWriteChan      chan message.CallMessage           // 调用写入通道
-	respWriteChan      chan message.ResponseMessage       // 响应写入通道
-	queryProxyMetaChan chan struct{}                      // 查询代理物模型消息通道
-	queryMetaChan      chan struct{}                      // 查询元信息通道
-	metaGotChan        chan struct{}                      // 收到元信息消息通道
-	queryOnce          sync.Once                          // 保证只查询一次元信息
-	onGetMetaOnce      sync.Once                          // 保证只响应一次元信息结果报文
-	quitOnce           sync.Once                          // 保证只退出一次
-	MetaInfo           message.MetaMessage                // 元信息
+type model struct {
+	net.Conn                                            // 原始连接
+	quit             chan struct{}                      // 退出writer的信号
+	localSubStateCh  chan updateSubTableMsg             // 更新本地状态订阅通道
+	localSubEventCh  chan updateSubTableMsg             // 更新本地事件订阅通道
+	remoteSubStateCh chan updateSubTableMsg             // 更新远端状态订阅通道, 会触发订阅状态报文发送
+	remoteSubEventCh chan updateSubTableMsg             // 更新远端事件订阅通道, 会触发订阅事件报文发送
+	querySubState    chan chan []string                 // 查询状态订阅通道
+	querySubEvent    chan chan []string                 // 查询事件订阅通道
+	removeConnCh     chan<- *model                      // 删除连接通道
+	stateBroadcast   chan<- message.StateOrEventMessage // 状态广播通道
+	eventBroadcast   chan<- message.StateOrEventMessage // 事件广播通道
+	callChan         chan<- message.CallMessage         // 调用请求通道
+	respChan         chan<- message.ResponseMessage     // 响应结果通道
+	serverDone       <-chan struct{}                    // Server 完成退出信息
+	stateWriteChan   chan message.StateOrEventMessage   // 状态写入通道
+	eventWriteChan   chan message.StateOrEventMessage   // 事件写入通道
+	callWriteChan    chan message.CallMessage           // 调用写入通道
+	respWriteChan    chan message.ResponseMessage       // 响应写入通道
+	writeChan        chan []byte                        // 数据写入通道
+	metaGotChan      chan struct{}                      // 收到元信息消息通道
+	queryOnce        sync.Once                          // 保证只查询一次元信息
+	onGetMetaOnce    sync.Once                          // 保证只响应一次元信息结果报文
+	quitOnce         sync.Once                          // 保证只退出一次
+	MetaInfo         message.MetaMessage                // 元信息
 
 }
 
-func (c *ModelConnection) Write(data []byte) (int, error) {
+func (m *model) Write(data []byte) (int, error) {
 	if len(data) == 0 {
 		return 0, nil
 	}
 
 	length := uint32(len(data))
-	err := binary.Write(c.Conn, binary.LittleEndian, &length)
+	err := binary.Write(m.Conn, binary.LittleEndian, &length)
 	if err != nil {
 		return 0, err
 	}
 
-	return c.Conn.Write(data)
+	return m.Conn.Write(data)
 }
 
-func (c *ModelConnection) GetSubStates() []string {
+func (m *model) GetSubStates() []string {
 	queryChan := make(chan []string, 1)
-	c.querySubState <- queryChan
+	m.querySubState <- queryChan
 	return <-queryChan
 }
 
-func (c *ModelConnection) SetSubStates(states []string) {
-	c.localSubStateCh <- updateSubTableMsg{
+func (m *model) SetSubStates(states []string) {
+	m.localSubStateCh <- updateSubTableMsg{
 		option: setSub,
 		items:  states,
 	}
 }
 
-func (c *ModelConnection) AddSubStates(states []string) {
-	c.localSubStateCh <- updateSubTableMsg{
+func (m *model) AddSubStates(states []string) {
+	m.localSubStateCh <- updateSubTableMsg{
 		option: addSub,
 		items:  states,
 	}
 }
 
-func (c *ModelConnection) RemoveSubStates(states []string) {
-	c.localSubStateCh <- updateSubTableMsg{
+func (m *model) RemoveSubStates(states []string) {
+	m.localSubStateCh <- updateSubTableMsg{
 		option: removeSub,
 		items:  states,
 	}
 }
 
-func (c *ModelConnection) ClearSubStates() {
-	c.localSubStateCh <- updateSubTableMsg{
+func (m *model) ClearSubStates() {
+	m.localSubStateCh <- updateSubTableMsg{
 		option: clearSub,
 	}
 }
 
-func (c *ModelConnection) GetSubEvents() []string {
+func (m *model) GetSubEvents() []string {
 	queryChan := make(chan []string, 1)
-	c.querySubEvent <- queryChan
+	m.querySubEvent <- queryChan
 	return <-queryChan
 }
 
-func (c *ModelConnection) SetSubEvents(events []string) {
-	c.localSubEventCh <- updateSubTableMsg{
+func (m *model) SetSubEvents(events []string) {
+	m.localSubEventCh <- updateSubTableMsg{
 		option: setSub,
 		items:  events,
 	}
 }
 
-func (c *ModelConnection) AddSubEvents(events []string) {
-	c.localSubEventCh <- updateSubTableMsg{
+func (m *model) AddSubEvents(events []string) {
+	m.localSubEventCh <- updateSubTableMsg{
 		option: addSub,
 		items:  events,
 	}
 }
 
-func (c *ModelConnection) RemoveSubEvents(events []string) {
-	c.localSubEventCh <- updateSubTableMsg{
+func (m *model) RemoveSubEvents(events []string) {
+	m.localSubEventCh <- updateSubTableMsg{
 		option: removeSub,
 		items:  events,
 	}
 }
 
-func (c *ModelConnection) ClearSubEvents() {
-	c.localSubEventCh <- updateSubTableMsg{
+func (m *model) ClearSubEvents() {
+	m.localSubEventCh <- updateSubTableMsg{
 		option: clearSub,
 	}
 }
 
-func (c *ModelConnection) quitWriter() {
-	c.quitOnce.Do(func() {
-		close(c.quit)
+func (m *model) quitWriter() {
+	m.quitOnce.Do(func() {
+		close(m.quit)
 	})
 }
 
-func (c *ModelConnection) queryMeta(timeout time.Duration) error {
-	c.queryOnce.Do(func() {
-		c.queryMetaChan <- struct{}{}
+func (m *model) queryMeta(timeout time.Duration) error {
+	m.queryOnce.Do(func() {
+		m.writeChan <- []byte(queryMetaJSON)
 	})
 
 	select {
 	case <-time.After(timeout):
 		return fmt.Errorf("timeout")
-	case <-c.metaGotChan:
+	case <-m.metaGotChan:
 		break
 	}
 
 	return nil
 }
 
-func (c *ModelConnection) reader() {
+func (m *model) reader() {
 	defer func() {
 		select {
-		case c.removeConnCh <- c: // 在Server未退出的情况下，通过Server退出writer
-		case <-c.serverDone:
+		case m.removeConnCh <- m: // 在Server未退出的情况下，通过Server退出writer
+		case <-m.serverDone:
 			// NOTE: 在Server完全退出的情况下，可自行退出writer
 			// NOTE: 否则会导致提前退出了writer的情况下，run还在向writer发送信号，
 			// NOTE: 从而导致run无法退出
-			c.quitWriter()
+			m.quitWriter()
 		}
 	}()
 	for {
 		// 读取报文
-		data, err := c.readMsg()
+		data, err := m.readMsg()
 		if err != nil {
 			break
 		}
@@ -204,66 +203,62 @@ func (c *ModelConnection) reader() {
 		}
 
 		// 处理包
-		if err = c.dealMsg(msg.Type, msg.Payload, data); err != nil {
+		if err = m.dealMsg(msg.Type, msg.Payload, data); err != nil {
 			break
 		}
 	}
 }
 
-func (c *ModelConnection) writer() {
-	defer c.Close()
+func (m *model) writer() {
+	defer m.Close()
 	wantedStates := make(map[string]int) // 链路期望接收的状态列表
 	wantedEvents := make(map[string]int) // 链路期望接收的事件列表
 	for {
 		select {
 		// 退出
-		case <-c.quit:
+		case <-m.quit:
 			// NOTE: 只有主动退出了才return, 其他情况忽略错误继续执行
 			// NOTE: 这样能保证通过通道向writer发数据时，不会因为writer退出而死锁！！！
 			return
 
-		// 查询连接对应的物模型元信息
-		case <-c.queryMetaChan:
-			_, _ = c.Write([]byte(queryMetaJSON))
-
-		// 查询代理对应的物模型元信息
-		case <-c.queryProxyMetaChan:
-			_, _ = c.Write([]byte(proxyMetaJSON))
-
 		// 状态发布
-		case state := <-c.stateWriteChan:
-			c.pubStateOrEvent(wantedStates, state)
+		case state := <-m.stateWriteChan:
+			m.pubStateOrEvent(wantedStates, state)
 
 		// 事件发布
-		case event := <-c.eventWriteChan:
-			c.pubStateOrEvent(wantedEvents, event)
+		case event := <-m.eventWriteChan:
+			m.pubStateOrEvent(wantedEvents, event)
 
 		// 调用消息
-		case call := <-c.callWriteChan:
-			_, _ = c.Write(call.FullData)
+		case call := <-m.callWriteChan:
+			_, _ = m.Write(call.FullData)
 
 		// 调用响应
-		case resp := <-c.respWriteChan:
-			_, _ = c.Write(resp.FullData)
+		case resp := <-m.respWriteChan:
+			_, _ = m.Write(resp.FullData)
+
+		// 发送数据
+		case data := <-m.writeChan:
+			_, _ = m.Write(data)
 
 		// 更新连接本地的订阅状态列表
-		case stateReq := <-c.localSubStateCh:
+		case stateReq := <-m.localSubStateCh:
 			wantedStates = dealSubReq(stateReq, wantedStates)
 
 		// 更新连接本地的订阅事件列表
-		case eventReq := <-c.localSubEventCh:
+		case eventReq := <-m.localSubEventCh:
 			wantedEvents = dealSubReq(eventReq, wantedEvents)
 
 		// 发送订阅状态报文
-		case stateReq := <-c.remoteSubStateCh:
-			c.sendSubStateMsg(stateReq)
+		case stateReq := <-m.remoteSubStateCh:
+			m.sendSubStateMsg(stateReq)
 
 		// 发送订阅事件报文
-		case eventReq := <-c.remoteSubEventCh:
-			c.sendSubEventMsg(eventReq)
+		case eventReq := <-m.remoteSubEventCh:
+			m.sendSubEventMsg(eventReq)
 
 		// 查询连接订阅状态列表
-		case resChan := <-c.querySubState:
+		case resChan := <-m.querySubState:
 			res := make([]string, 0, len(wantedStates))
 			for state := range wantedStates {
 				res = append(res, state)
@@ -271,7 +266,7 @@ func (c *ModelConnection) writer() {
 			resChan <- res
 
 		// 查询连接订阅事件列表
-		case resChan := <-c.querySubEvent:
+		case resChan := <-m.querySubEvent:
 			res := make([]string, 0, len(wantedEvents))
 			for event := range wantedEvents {
 				res = append(res, event)
@@ -281,50 +276,50 @@ func (c *ModelConnection) writer() {
 	}
 }
 
-func (c *ModelConnection) readMsg() ([]byte, error) {
+func (m *model) readMsg() ([]byte, error) {
 	// 读取长度
 	var length uint32
-	err := binary.Read(c, binary.LittleEndian, &length)
+	err := binary.Read(m, binary.LittleEndian, &length)
 	if err != nil {
 		return nil, err
 	}
 
 	// 读取数据
 	data := make([]byte, length)
-	if err = binary.Read(c, binary.LittleEndian, &data); err != nil {
+	if err = binary.Read(m, binary.LittleEndian, &data); err != nil {
 		return nil, err
 	}
 	return data, nil
 }
 
-func (c *ModelConnection) pubStateOrEvent(pubSet map[string]int, state message.StateOrEventMessage) {
+func (m *model) pubStateOrEvent(pubSet map[string]int, state message.StateOrEventMessage) {
 	if _, seen := pubSet[state.Name]; !seen {
 		return
 	}
-	_, _ = c.Write(state.FullData)
+	_, _ = m.Write(state.FullData)
 	return
 }
 
-func (c *ModelConnection) dealMsg(msgType string, payload []byte, fullData []byte) error {
+func (m *model) dealMsg(msgType string, payload []byte, fullData []byte) error {
 	switch msgType {
 	case "set-subscribe-state", "add-subscribe-state",
 		"remove-subscribe-state", "clear-subscribe-state":
-		return c.onSubState(msgType, payload)
+		return m.onSubState(msgType, payload)
 	case "set-subscribe-event", "add-subscribe-event",
 		"remove-subscribe-event", "clear-subscribe-event":
-		return c.onSubEvent(msgType, payload)
+		return m.onSubEvent(msgType, payload)
 	case "state":
-		return c.onState(payload, fullData)
+		return m.onState(payload, fullData)
 	case "event":
-		return c.onEvent(payload, fullData)
+		return m.onEvent(payload, fullData)
 	case "call":
-		return c.onCall(payload, fullData)
+		return m.onCall(payload, fullData)
 	case "response":
-		return c.onResp(payload, fullData)
+		return m.onResp(payload, fullData)
 	case "query-meta":
-		return c.onQueryMeta()
+		return m.onQueryMeta()
 	case "meta-info":
-		return c.onMetaInfo(payload, fullData)
+		return m.onMetaInfo(payload, fullData)
 	default:
 		return fmt.Errorf("invalid message type %s", msgType)
 	}
@@ -332,7 +327,7 @@ func (c *ModelConnection) dealMsg(msgType string, payload []byte, fullData []byt
 	return nil
 }
 
-func (c *ModelConnection) onSubState(Type string, payload []byte) error {
+func (m *model) onSubState(Type string, payload []byte) error {
 	var states []string
 	if err := jsoniter.Unmarshal(payload, &states); err != nil {
 		return err
@@ -340,19 +335,19 @@ func (c *ModelConnection) onSubState(Type string, payload []byte) error {
 
 	switch Type {
 	case "set-subscribe-state":
-		c.SetSubStates(states)
+		m.SetSubStates(states)
 	case "add-subscribe-state":
-		c.AddSubStates(states)
+		m.AddSubStates(states)
 	case "remove-subscribe-state":
-		c.RemoveSubStates(states)
+		m.RemoveSubStates(states)
 	case "clear-subscribe-state":
-		c.ClearSubStates()
+		m.ClearSubStates()
 	}
 
 	return nil
 }
 
-func (c *ModelConnection) onSubEvent(Type string, payload []byte) error {
+func (m *model) onSubEvent(Type string, payload []byte) error {
 	var events []string
 	if err := jsoniter.Unmarshal(payload, &events); err != nil {
 		return err
@@ -360,57 +355,57 @@ func (c *ModelConnection) onSubEvent(Type string, payload []byte) error {
 
 	switch Type {
 	case "set-subscribe-event":
-		c.SetSubEvents(events)
+		m.SetSubEvents(events)
 	case "add-subscribe-":
-		c.AddSubEvents(events)
+		m.AddSubEvents(events)
 	case "remove-subscribe-":
-		c.RemoveSubEvents(events)
+		m.RemoveSubEvents(events)
 	case "clear-subscribe-event":
-		c.ClearSubEvents()
+		m.ClearSubEvents()
 	}
 
 	return nil
 }
 
-func (c *ModelConnection) onState(payload []byte, fullData []byte) error {
+func (m *model) onState(payload []byte, fullData []byte) error {
 	var state message.StatePayload
 	if err := jsoniter.Unmarshal(payload, &state); err != nil {
 		return err
 	}
 
 	select {
-	case c.stateBroadcast <- message.StateOrEventMessage{
-		Source:   c.MetaInfo.Name,
+	case m.stateBroadcast <- message.StateOrEventMessage{
+		Source:   m.MetaInfo.Name,
 		Name:     state.Name,
 		FullData: fullData,
 	}:
-	case <-c.serverDone:
+	case <-m.serverDone:
 		return fmt.Errorf("proxy have quit")
 	}
 
 	return nil
 }
 
-func (c *ModelConnection) onEvent(payload []byte, fullData []byte) error {
+func (m *model) onEvent(payload []byte, fullData []byte) error {
 	var event message.EventPayload
 	if err := jsoniter.Unmarshal(payload, &event); err != nil {
 		return err
 	}
 
 	select {
-	case c.eventBroadcast <- message.StateOrEventMessage{
-		Source:   c.MetaInfo.Name,
+	case m.eventBroadcast <- message.StateOrEventMessage{
+		Source:   m.MetaInfo.Name,
 		Name:     event.Name,
 		FullData: fullData,
 	}:
-	case <-c.serverDone:
+	case <-m.serverDone:
 		return fmt.Errorf("proxy have quit")
 	}
 
 	return nil
 }
 
-func (c *ModelConnection) onCall(payload []byte, fullData []byte) error {
+func (m *model) onCall(payload []byte, fullData []byte) error {
 	var call message.CallPayload
 	if err := jsoniter.Unmarshal(payload, &call); err != nil {
 		return err
@@ -419,24 +414,20 @@ func (c *ModelConnection) onCall(payload []byte, fullData []byte) error {
 	modelName, err := splitModelName(call.Name)
 	if err != nil {
 		resp := make(map[string]interface{})
-		c.respWriteChan <- message.ResponseMessage{
-			Source:   "proxy",
-			UUID:     call.UUID,
-			FullData: message.NewResponseFullData(call.UUID, err.Error(), resp),
-		}
+		m.writeChan <- message.NewResponseFullData(call.UUID, err.Error(), resp)
 		return nil
 	}
 
 	select {
-	case c.callChan <- message.CallMessage{
-		Source:   c.MetaInfo.Name,
+	case m.callChan <- message.CallMessage{
+		Source:   m.MetaInfo.Name,
 		Model:    modelName,
 		UUID:     call.UUID,
 		FullData: fullData,
 	}:
-	case <-c.serverDone:
+	case <-m.serverDone:
 		resp := make(map[string]interface{})
-		c.respWriteChan <- message.ResponseMessage{
+		m.respWriteChan <- message.ResponseMessage{
 			Source:   "proxy",
 			UUID:     call.UUID,
 			FullData: message.NewResponseFullData(call.UUID, "proxy have quit", resp),
@@ -447,43 +438,43 @@ func (c *ModelConnection) onCall(payload []byte, fullData []byte) error {
 	return nil
 }
 
-func (c *ModelConnection) onResp(payload []byte, fullData []byte) error {
+func (m *model) onResp(payload []byte, fullData []byte) error {
 	var resp message.ResponsePayload
 	if err := jsoniter.Unmarshal(payload, &resp); err != nil {
 		return err
 	}
 
 	select {
-	case c.respChan <- message.ResponseMessage{
-		Source:   c.MetaInfo.Name,
+	case m.respChan <- message.ResponseMessage{
+		Source:   m.MetaInfo.Name,
 		UUID:     resp.UUID,
 		FullData: fullData,
 	}:
-	case <-c.serverDone:
+	case <-m.serverDone:
 		return fmt.Errorf("proxy have quit")
 	}
 
 	return nil
 }
 
-func (c *ModelConnection) onQueryMeta() error {
-	c.queryProxyMetaChan <- struct{}{}
+func (m *model) onQueryMeta() error {
+	m.writeChan <- []byte(proxyMetaJSON)
 	return nil
 }
 
-func (c *ModelConnection) onMetaInfo(payload []byte, fullData []byte) error {
+func (m *model) onMetaInfo(payload []byte, fullData []byte) error {
 	var metaInfo meta.Meta
 	if err := jsoniter.Unmarshal(payload, &metaInfo); err != nil {
 		return err
 	}
 
-	c.onGetMetaOnce.Do(func() {
-		c.MetaInfo = message.MetaMessage{
+	m.onGetMetaOnce.Do(func() {
+		m.MetaInfo = message.MetaMessage{
 			Meta:     metaInfo,
 			FullData: fullData,
 		}
 
-		close(c.metaGotChan)
+		close(m.metaGotChan)
 	})
 
 	return nil
@@ -511,7 +502,7 @@ func dealSubReq(req updateSubTableMsg, pubSet map[string]int) map[string]int {
 	return pubSet
 }
 
-func (c *ModelConnection) sendSubStateMsg(req updateSubTableMsg) {
+func (m *model) sendSubStateMsg(req updateSubTableMsg) {
 	msg := message.Message{}
 	switch req.option {
 	case setSub:
@@ -529,10 +520,10 @@ func (c *ModelConnection) sendSubStateMsg(req updateSubTableMsg) {
 	msg.Payload, _ = jsoniter.Marshal(req.items)
 	data, _ := jsoniter.Marshal(msg)
 
-	_, _ = c.Write(data)
+	_, _ = m.Write(data)
 }
 
-func (c *ModelConnection) sendSubEventMsg(req updateSubTableMsg) {
+func (m *model) sendSubEventMsg(req updateSubTableMsg) {
 	msg := message.Message{}
 	switch req.option {
 	case setSub:
@@ -550,7 +541,7 @@ func (c *ModelConnection) sendSubEventMsg(req updateSubTableMsg) {
 	msg.Payload, _ = jsoniter.Marshal(req.items)
 	data, _ := jsoniter.Marshal(msg)
 
-	_, _ = c.Write(data)
+	_, _ = m.Write(data)
 }
 
 func splitModelName(fullName string) (string, error) {
--- server/server.go
+++ server/server.go
@@ -8,8 +8,8 @@ import (
 )
 
 type Server struct {
-	addConnChan    chan *ModelConnection
-	removeConnChan chan *ModelConnection
+	addConnChan    chan *model
+	removeConnChan chan *model
 	stateChan      chan message.StateOrEventMessage
 	eventChan      chan message.StateOrEventMessage
 	callChan       chan message.CallMessage
@@ -20,8 +20,8 @@ type Server struct {
 
 func New() *Server {
 	return &Server{
-		addConnChan:    make(chan *ModelConnection),
-		removeConnChan: make(chan *ModelConnection),
+		addConnChan:    make(chan *model),
+		removeConnChan: make(chan *model),
 		stateChan:      make(chan message.StateOrEventMessage),
 		eventChan:      make(chan message.StateOrEventMessage),
 		callChan:       make(chan message.CallMessage),
@@ -29,6 +29,11 @@ func New() *Server {
 		quitCh:         make(chan struct{}),
 		done:           make(chan struct{}),
 	}
+}
+
+type connection struct {
+	*model
+	inCalls map[string]struct{} // 所有发给自己的调用请求的UUID
 }
 
 func (s *Server) ListenServe(addr string) error {
@@ -52,8 +57,8 @@ func (s *Server) ListenServe(addr string) error {
 
 func (s *Server) run() {
 	defer close(s.done)
-	connections := make(map[string]*ModelConnection) // 所有连接
-	respWaiters := make(map[string]string)           // 等待响应的所有连接，uuid -> 链路的物模型名称
+	connections := make(map[string]connection) // 所有连接
+	respWaiters := make(map[string]string)     // 等待响应的所有连接，uuid -> 发送调用请求的物模型名称
 	for {
 		select {
 		case <-s.quitCh:
@@ -87,17 +92,14 @@ func (s *Server) run() {
 			if conn, seen := connections[call.Model]; !seen {
 				errStr := fmt.Sprintf("model %q NOT exist", call.Model)
 				resp := make(map[string]interface{})
-				connections[call.Source].respWriteChan <- message.ResponseMessage{
-					Source:   "proxy",
-					UUID:     call.UUID,
-					FullData: message.NewResponseFullData(call.UUID, errStr, resp),
-				}
+				connections[call.Source].writeChan <- message.NewResponseFullData(call.UUID, errStr, resp)
 				continue
 			} else {
 				// 转发调用请求
 				conn.callWriteChan <- call
 				// 记录调用请求
 				respWaiters[call.UUID] = call.Source
+				conn.inCalls[call.UUID] = struct{}{}
 			}
 		case resp := <-s.respChan:
 			// 不是在编的物模型连接发送的调用请求不响应
@@ -110,56 +112,70 @@ func (s *Server) run() {
 			}
 			// 转发调用请求, 必须判断等待调用请求的连接是否还在线
 			if destConn, seen := connections[respWaiters[resp.UUID]]; seen {
-				destConn.respWriteChan <- resp
+				destConn.writeChan <- resp.FullData
 			}
 
 			// 删除调用记录
 			delete(respWaiters, resp.UUID)
-		case conn := <-s.addConnChan:
+		case model := <-s.addConnChan:
 			// 订阅所有状态
-			conn.remoteSubStateCh <- updateSubTableMsg{
+			model.remoteSubStateCh <- updateSubTableMsg{
 				option: setSub,
-				items:  conn.MetaInfo.AllStates(),
+				items:  model.MetaInfo.AllStates(),
 			}
 			// 订阅所有事件
-			conn.remoteSubEventCh <- updateSubTableMsg{
+			model.remoteSubEventCh <- updateSubTableMsg{
 				option: setSub,
-				items:  conn.MetaInfo.AllEvents(),
+				items:  model.MetaInfo.AllEvents(),
 			}
 			// 添加链路
-			connections[conn.MetaInfo.Name] = conn
-		case conn := <-s.removeConnChan:
-			delete(connections, conn.MetaInfo.Name)
+			connections[model.MetaInfo.Name] = connection{
+				model:   model,
+				inCalls: make(map[string]struct{}),
+			}
+		case model := <-s.removeConnChan:
+			// 通知所有等待本连接响应报文的调用请求 可以不用等了
+			if conn, seen := connections[model.MetaInfo.Name]; seen {
+				errStr := fmt.Sprintf("model %q have quit", model.MetaInfo.Name)
+				empty := make(map[string]interface{})
+				for uuid := range conn.inCalls {
+					if destConn, ok := connections[respWaiters[uuid]]; ok {
+						destConn.writeChan <- message.NewResponseFullData(uuid, errStr, empty)
+					}
+				}
+			}
+
+			delete(connections, model.MetaInfo.Name)
+
 			// NOTE: 在此处quitWriter, 不会导致由于连接writer协程提前退出而导致的死锁
 			// NOTE: 因为只有调用了quitWriter之后，writer协程才会退出
-			conn.quitWriter()
+			model.quitWriter()
 		}
 	}
 }
 
 func (s *Server) addModelConnection(conn net.Conn) {
-	ans := &ModelConnection{
-		Conn:               conn,
-		localSubStateCh:    make(chan updateSubTableMsg),
-		localSubEventCh:    make(chan updateSubTableMsg),
-		remoteSubStateCh:   make(chan updateSubTableMsg),
-		remoteSubEventCh:   make(chan updateSubTableMsg),
-		querySubState:      make(chan chan []string),
-		querySubEvent:      make(chan chan []string),
-		removeConnCh:       s.removeConnChan,
-		stateBroadcast:     s.stateChan,
-		eventBroadcast:     s.eventChan,
-		callChan:           s.callChan,
-		respChan:           s.respChan,
-		serverDone:         s.done,
-		stateWriteChan:     make(chan message.StateOrEventMessage, 256),
-		eventWriteChan:     make(chan message.StateOrEventMessage, 256),
-		callWriteChan:      make(chan message.CallMessage, 256),
-		respWriteChan:      make(chan message.ResponseMessage, 256),
-		quit:               make(chan struct{}),
-		queryProxyMetaChan: make(chan struct{}),
-		queryMetaChan:      make(chan struct{}),
-		metaGotChan:        make(chan struct{}),
+	ans := &model{
+		Conn:             conn,
+		localSubStateCh:  make(chan updateSubTableMsg),
+		localSubEventCh:  make(chan updateSubTableMsg),
+		remoteSubStateCh: make(chan updateSubTableMsg),
+		remoteSubEventCh: make(chan updateSubTableMsg),
+		querySubState:    make(chan chan []string),
+		querySubEvent:    make(chan chan []string),
+		removeConnCh:     s.removeConnChan,
+		stateBroadcast:   s.stateChan,
+		eventBroadcast:   s.eventChan,
+		callChan:         s.callChan,
+		respChan:         s.respChan,
+		serverDone:       s.done,
+		stateWriteChan:   make(chan message.StateOrEventMessage, 256),
+		eventWriteChan:   make(chan message.StateOrEventMessage, 256),
+		callWriteChan:    make(chan message.CallMessage, 256),
+		respWriteChan:    make(chan message.ResponseMessage, 256),
+		writeChan:        make(chan []byte, 256),
+		quit:             make(chan struct{}),
+		metaGotChan:      make(chan struct{}),
 	}
 
 	go ans.writer()
@@ -171,13 +187,14 @@ func (s *Server) addModelConnection(conn net.Conn) {
 		// NOTE: 这样保证链路协程的退出顺序始终为：
 		// NOTE: Close() -> reader退出 —> 向Server发出链路退出信号 ->
 		// NOTE: 关闭链路quit通道 -> writer退出
-		ans.Close()
+		_ = ans.Close()
 		return
 	}
 
 	// TODO: 添加元信息校验，元信息校验不通过则不添加, 并退出
 	// if ans.MetaInfo.Check() != nil {
-	// 	ans.Close()
+	//    ans.Close()
+	//    return
 	// }
 
 	// 添加链路
