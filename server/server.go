c9d07a745fe0903dbc5c6c5ed723a631604ce248
hulele <hulelebuaa@163.com>
2023-11-07 13:21:04 +0800

    优化server的run函数结构

--- server/server.go
+++ server/server.go
@@ -74,220 +74,226 @@ func (s *Server) run() {
 	respWaiters := make(map[string]string)
 	for {
 		select {
-		// 状态
 		case state := <-s.stateChan:
 			for _, conn := range connections {
 				if _, want := conn.pubStates[state.Name]; want {
 					conn.writeChan <- state.FullData
 				}
 			}
-
-		// 事件
 		case event := <-s.eventChan:
 			for _, conn := range connections {
 				if _, want := conn.pubEvents[event.Name]; want {
 					conn.writeChan <- event.FullData
 				}
 			}
-
-		// 调用
 		case call := <-s.callChan:
-			if call.Model == "proxy" {
-				// 调用代理的方法
-				go s.dealProxyCall(call, connections[call.Source])
-			} else if conn, seen := connections[call.Model]; !seen {
-				// 期望调用的物模型不存在，直接返回错误响应
-				errStr := fmt.Sprintf("model %q NOT exist", call.Model)
-				resp := make(map[string]interface{})
-				connections[call.Source].writeChan <- message.NewResponseFullData(call.UUID, errStr, resp)
-				continue
-			} else {
-				// 转发调用请求
-				conn.writeChan <- call.FullData
-
-				// 记录调用请求
-				respWaiters[call.UUID] = call.Source
-				conn.inCalls[call.UUID] = struct{}{}
-				connections[call.Source].outCalls[call.UUID] = struct{}{}
-			}
-
-		// 响应报文
+			s.onCall(call, connections, respWaiters)
 		case resp := <-s.respChan:
-			// 不是在编的物模型连接发送的调用请求不响应
-			if srcConn, seen := connections[resp.Source]; !seen {
-				continue
-			} else {
-				delete(srcConn.inCalls, resp.UUID)
-			}
-			// 响应无调用请求
-			if _, seen := respWaiters[resp.UUID]; !seen {
-				continue
-			}
-			// 转发调用请求, 清空调用记录，必须判断等待调用请求的连接是否还在线
-			if destConn, seen := connections[respWaiters[resp.UUID]]; seen {
-				destConn.writeChan <- resp.FullData
-				delete(destConn.outCalls, resp.UUID)
-			}
-			// 删除调用记录
-			delete(respWaiters, resp.UUID)
-
-		// 更新状态发布
+			onResp(connections, resp, respWaiters)
 		case subStateReq := <-s.subStateChan:
 			if conn, seen := connections[subStateReq.Source]; seen {
 				conn.pubStates = message.UpdatePubTable(subStateReq, conn.pubStates)
 				connections[subStateReq.Source] = conn
 			}
-
-		// 更新状态发布
 		case subEventReq := <-s.subEventChan:
 			if conn, seen := connections[subEventReq.Source]; seen {
 				conn.pubEvents = message.UpdatePubTable(subEventReq, conn.pubEvents)
 				connections[subEventReq.Source] = conn
 			}
-
-		// 添加链路
 		case m := <-s.addConnChan:
-			// 模型名称重复，直接关闭连接
-			if _, repeat := connections[m.MetaInfo.Name]; repeat {
-				_ = m.Close()
-				continue
-			}
-			// 订阅所有状态
-			data, _ := message.NewPubStateMessage(message.SetSub, m.MetaInfo.AllStates())
-			m.writeChan <- data
-
-			// 订阅所有事件
-			data, _ = message.NewPubEventMessage(message.SetSub, m.MetaInfo.AllEvents())
-			m.writeChan <- data
-
-			conn := connection{
-				model:     m,
-				outCalls:  map[string]struct{}{},
-				inCalls:   map[string]struct{}{},
-				pubStates: map[string]struct{}{},
-				pubEvents: map[string]struct{}{},
-			}
-
-			// 推送上线事件
-			go s.PushOnlineOrOfflineEvent(m.MetaInfo.Name, m.RemoteAddr().String(), true)
-
-			// 添加链路, 并通知已添加
-			connections[m.MetaInfo.Name] = conn
-			m.setAdded()
-
-		// 删除链路
+			s.onAddConn(connections, m)
 		case m := <-s.removeConnChan:
-			if conn, seen := connections[m.MetaInfo.Name]; seen {
-				// 通知所有等待本连接响应报文的调用请求 可以不用等了
-				errStr := fmt.Sprintf("model %q have quit", m.MetaInfo.Name)
-				empty := make(map[string]interface{})
-				for uuid := range conn.inCalls {
-					if destConn, ok := connections[respWaiters[uuid]]; ok {
-						destConn.writeChan <- message.NewResponseFullData(uuid, errStr, empty)
-					}
-				}
-
-				// 清空本连接的等待的所有调用
-				for uuid := range conn.outCalls {
-					delete(respWaiters, uuid)
-				}
-
-				// 删除链路
-				delete(connections, m.MetaInfo.Name)
-
-				// 推送下线事件
-				go s.PushOnlineOrOfflineEvent(m.MetaInfo.Name, m.RemoteAddr().String(), false)
-			}
-
-			// NOTE: 在此处quitWriter, 不会导致由于连接writer协程提前退出而导致的死锁
-			// NOTE: 因为只有调用了quitWriter之后，writer协程才会退出
-			m.quitWriter()
-
-		// 查询所有在线物模型
+			s.onRemoveConn(connections, m, respWaiters)
 		case resChan := <-s.queryAllModel:
-			items := make([]modelItem, 0, len(connections))
-			for modelName, conn := range connections {
-				states := make([]string, 0, len(conn.pubStates))
-				events := make([]string, 0, len(conn.pubEvents))
-				for state := range conn.pubStates {
-					states = append(states, state)
-				}
-				for event := range conn.pubEvents {
-					events = append(events, event)
-				}
-				items = append(items, modelItem{
-					ModelName: modelName,
-					Addr:      conn.model.RemoteAddr().String(),
-					SubStates: states,
-					SubEvents: events,
-					MetaInfo:  conn.MetaInfo.FullData,
-				})
-			}
-			resChan <- items
-
-		// 查询指定物模型
+			onQueryAllModel(connections, resChan)
 		case queryModel := <-s.queryModel:
-			info := modelItem{
-				ModelName: "none",
-				Addr:      "",
-				SubStates: make([]string, 0),
-				SubEvents: make([]string, 0),
-				MetaInfo:  []byte(noneMetaJSON),
-			}
-			conn, seen := connections[queryModel.ModelName]
-			if seen {
-				info.ModelName = conn.MetaInfo.Name
-				info.SubStates = make([]string, 0, len(conn.pubStates))
-				for state := range conn.pubStates {
-					info.SubStates = append(info.SubStates, state)
-				}
-				info.SubEvents = make([]string, 0, len(conn.pubEvents))
-				for state := range conn.pubEvents {
-					info.SubEvents = append(info.SubEvents, state)
-				}
-				info.Addr = conn.RemoteAddr().String()
-				info.MetaInfo = conn.MetaInfo.FullData
-			}
-			queryModel.ResChan <- queryModelRes{
-				ModelInfo: info,
-				Got:       seen,
-			}
-
-		// 查询物模型是否在线
+			onQueryModel(connections, queryModel)
 		case isOnlineReq := <-s.queryOnline:
 			_, seen := connections[isOnlineReq.ModelName]
 			isOnlineReq.ResChan <- seen
-
-		// 查询模型状态订阅关系
 		case querySubState := <-s.querySubState:
-			subList := make([]string, 0)
-			conn, seen := connections[querySubState.ModelName]
-			if seen {
-				subList = make([]string, 0, len(conn.pubStates))
-				for pub := range conn.pubStates {
-					subList = append(subList, pub)
-				}
-			}
-			querySubState.ResChan <- querySubRes{
-				SubList: subList,
-				Got:     seen,
-			}
-
-		// 查询模型状态订阅关系
+			onQuerySub(connections, querySubState, true)
 		case querySubEvent := <-s.querySubEvent:
-			subList := make([]string, 0)
-			conn, seen := connections[querySubEvent.ModelName]
-			if seen {
-				subList = make([]string, 0, len(conn.pubEvents))
-				for pub := range conn.pubEvents {
-					subList = append(subList, pub)
-				}
-			}
-			querySubEvent.ResChan <- querySubRes{
-				SubList: subList,
-				Got:     seen,
+			onQuerySub(connections, querySubEvent, false)
+		}
+	}
+}
+
+func (s *Server) onCall(call message.CallMessage,
+	connections map[string]connection,
+	respWaiters map[string]string) {
+	if call.Model == "proxy" {
+		// 调用代理的方法
+		go s.dealProxyCall(call, connections[call.Source])
+		return
+	}
+
+	conn, seen := connections[call.Model]
+	if !seen {
+		// 期望调用的物模型不存在，直接返回错误响应
+		errStr := fmt.Sprintf("model %q NOT exist", call.Model)
+		resp := make(map[string]interface{})
+		connections[call.Source].writeChan <- message.NewResponseFullData(call.UUID, errStr, resp)
+		return
+	}
+
+	// 转发调用请求
+	conn.writeChan <- call.FullData
+
+	// 记录调用请求
+	respWaiters[call.UUID] = call.Source
+	conn.inCalls[call.UUID] = struct{}{}
+	connections[call.Source].outCalls[call.UUID] = struct{}{}
+}
+
+func onResp(connections map[string]connection, resp message.ResponseMessage,
+	respWaiters map[string]string) {
+	// 不是在编的物模型连接发送的调用请求不响应
+	if srcConn, seen := connections[resp.Source]; !seen {
+		return
+	} else {
+		delete(srcConn.inCalls, resp.UUID)
+	}
+	// 响应无调用请求
+	if _, seen := respWaiters[resp.UUID]; !seen {
+		return
+	}
+	// 转发调用请求, 清空调用记录，必须判断等待调用请求的连接是否还在线
+	if destConn, seen := connections[respWaiters[resp.UUID]]; seen {
+		destConn.writeChan <- resp.FullData
+		delete(destConn.outCalls, resp.UUID)
+	}
+	// 删除调用记录
+	delete(respWaiters, resp.UUID)
+}
+
+func (s *Server) onAddConn(connections map[string]connection, m *model) {
+	// 模型名称重复，直接关闭连接
+	if _, repeat := connections[m.MetaInfo.Name]; repeat {
+		_ = m.Close()
+		return
+	}
+	// 订阅所有状态
+	data, _ := message.NewPubStateMessage(message.SetSub, m.MetaInfo.AllStates())
+	m.writeChan <- data
+
+	// 订阅所有事件
+	data, _ = message.NewPubEventMessage(message.SetSub, m.MetaInfo.AllEvents())
+	m.writeChan <- data
+
+	conn := connection{
+		model:     m,
+		outCalls:  map[string]struct{}{},
+		inCalls:   map[string]struct{}{},
+		pubStates: map[string]struct{}{},
+		pubEvents: map[string]struct{}{},
+	}
+
+	// 推送上线事件
+	go s.PushOnlineOrOfflineEvent(m.MetaInfo.Name, m.RemoteAddr().String(), true)
+
+	// 添加链路, 并通知已添加
+	connections[m.MetaInfo.Name] = conn
+	m.setAdded()
+}
+
+func (s *Server) onRemoveConn(connections map[string]connection, m *model,
+	respWaiters map[string]string) {
+	if conn, seen := connections[m.MetaInfo.Name]; seen {
+		// 通知所有等待本连接响应报文的调用请求 可以不用等了
+		errStr := fmt.Sprintf("model %q have quit", m.MetaInfo.Name)
+		empty := make(map[string]interface{})
+		for uuid := range conn.inCalls {
+			if destConn, ok := connections[respWaiters[uuid]]; ok {
+				destConn.writeChan <- message.NewResponseFullData(uuid, errStr, empty)
 			}
 		}
+
+		// 清空本连接的等待的所有调用
+		for uuid := range conn.outCalls {
+			delete(respWaiters, uuid)
+		}
+
+		// 删除链路
+		delete(connections, m.MetaInfo.Name)
+
+		// 推送下线事件
+		go s.PushOnlineOrOfflineEvent(m.MetaInfo.Name, m.RemoteAddr().String(), false)
+	}
+
+	// NOTE: 在此处quitWriter, 不会导致由于连接writer协程提前退出而导致的死锁
+	// NOTE: 因为只有调用了quitWriter之后，writer协程才会退出
+	m.quitWriter()
+}
+
+func onQueryAllModel(connections map[string]connection, resChan chan []modelItem) {
+	items := make([]modelItem, 0, len(connections))
+	for modelName, conn := range connections {
+		states := make([]string, 0, len(conn.pubStates))
+		events := make([]string, 0, len(conn.pubEvents))
+		for state := range conn.pubStates {
+			states = append(states, state)
+		}
+		for event := range conn.pubEvents {
+			events = append(events, event)
+		}
+		items = append(items, modelItem{
+			ModelName: modelName,
+			Addr:      conn.model.RemoteAddr().String(),
+			SubStates: states,
+			SubEvents: events,
+			MetaInfo:  conn.MetaInfo.FullData,
+		})
+	}
+	resChan <- items
+}
+
+func onQueryModel(connections map[string]connection, queryModel queryModelReq) {
+	info := modelItem{
+		ModelName: "none",
+		Addr:      "",
+		SubStates: make([]string, 0),
+		SubEvents: make([]string, 0),
+		MetaInfo:  []byte(noneMetaJSON),
+	}
+	conn, seen := connections[queryModel.ModelName]
+	if seen {
+		info.ModelName = conn.MetaInfo.Name
+		info.SubStates = make([]string, 0, len(conn.pubStates))
+		for state := range conn.pubStates {
+			info.SubStates = append(info.SubStates, state)
+		}
+		info.SubEvents = make([]string, 0, len(conn.pubEvents))
+		for state := range conn.pubEvents {
+			info.SubEvents = append(info.SubEvents, state)
+		}
+		info.Addr = conn.RemoteAddr().String()
+		info.MetaInfo = conn.MetaInfo.FullData
+	}
+	queryModel.ResChan <- queryModelRes{
+		ModelInfo: info,
+		Got:       seen,
+	}
+}
+
+func onQuerySub(connections map[string]connection, querySubState querySubReq, isState bool) {
+	subList := make([]string, 0)
+	conn, seen := connections[querySubState.ModelName]
+	if seen {
+		var subMap map[string]struct{}
+		if isState {
+			subMap = conn.pubStates
+		} else {
+			subMap = conn.pubEvents
+		}
+		subList = make([]string, 0, len(subMap))
+		for pub := range subMap {
+			subList = append(subList, pub)
+		}
+	}
+	querySubState.ResChan <- querySubRes{
+		SubList: subList,
+		Got:     seen,
 	}
 }
 
