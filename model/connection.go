b2bee33a62d6548229db6c8520ac350bff358b87
hulele <hulelebuaa@163.com>
2023-12-11 21:10:15 +0800

    完成状态和事件报文的处理

--- model/connection.go
+++ model/connection.go
@@ -4,26 +4,51 @@ import (
 	jsoniter "github.com/json-iterator/go"
 	"goModel/message"
 	"goModel/rawConn"
+	"strings"
 	"sync"
 )
 
+// StateFunc 为状态回调函数, 参数modelName为状态报文对应的物模型名称,
+// stateName 为状态报文对应的状态名, 参数data为状态数据.
+type StateFunc func(modelName string, stateName string, data []byte)
+
+// EventFunc 为事件回调函数, 参数modelName为事件报文对应的物模型名称,
+// 参数eventName 为事件报文对应的状态名, 参数args为事件所携带参数.
+type EventFunc func(modelName string, eventName string, args message.RawArgs)
+
 type Connection struct {
-	m           *Model
-	writeLock   sync.Mutex              // 写入锁, 保护 raw
-	raw         rawConn.RawConn         // 原始连接
-	msgHandlers map[string]func([]byte) // 报文处理函数
-	statesLock  sync.RWMutex            // 保护 pubStates
-	pubStates   map[string]struct{}     // 发布状态列表
-	eventsLock  sync.RWMutex            // 保护 pubEvents
-	pubEvents   map[string]struct{}     // 发布事件列表
+	m            *Model
+	writeLock    sync.Mutex                // 写入锁, 保护 raw
+	raw          rawConn.RawConn           // 原始连接
+	msgHandlers  map[string]func([]byte)   // 报文处理函数
+	statesLock   sync.RWMutex              // 保护 pubStates
+	pubStates    map[string]struct{}       // 发布状态列表
+	eventsLock   sync.RWMutex              // 保护 pubEvents
+	pubEvents    map[string]struct{}       // 发布事件列表
+	statesChan   chan message.StatePayload // 状态管道
+	eventsChan   chan message.EventPayload // 事件管道
+	stateHandler StateFunc                 // 状态处理回调
+	eventHandler EventFunc                 // 事件处理回调
 }
 
-func newConn(m *Model, raw rawConn.RawConn) *Connection {
+func newConn(m *Model, raw rawConn.RawConn, stateFunc StateFunc, eventFunc EventFunc) *Connection {
+	if stateFunc == nil {
+		stateFunc = func(string, string, []byte) {}
+	}
+
+	if eventFunc == nil {
+		eventFunc = func(string, string, message.RawArgs) {}
+	}
+
 	ans := &Connection{
-		m:         m,
-		raw:       raw,
-		pubStates: make(map[string]struct{}),
-		pubEvents: make(map[string]struct{}),
+		m:            m,
+		raw:          raw,
+		pubStates:    make(map[string]struct{}),
+		pubEvents:    make(map[string]struct{}),
+		statesChan:   make(chan message.StatePayload, 256),
+		eventsChan:   make(chan message.EventPayload, 256),
+		stateHandler: stateFunc,
+		eventHandler: eventFunc,
 	}
 
 	ans.msgHandlers = map[string]func([]byte){
@@ -42,6 +67,8 @@ func newConn(m *Model, raw rawConn.RawConn) *Connection {
 		"query-meta":             ans.onQueryMeta,
 		"meta-info":              ans.onMetaInfo,
 	}
+
+	go ans.dealState()
 
 	return ans
 }
@@ -109,11 +136,21 @@ func (conn *Connection) onClearSubEvent(payload []byte) {
 }
 
 func (conn *Connection) onState(payload []byte) {
-
+	state := message.StatePayload{}
+	json := jsoniter.ConfigCompatibleWithStandardLibrary
+	if json.Unmarshal(payload, &state) != nil {
+		return
+	}
+	conn.statesChan <- state
 }
 
 func (conn *Connection) onEvent(payload []byte) {
-
+	event := message.EventPayload{}
+	json := jsoniter.ConfigCompatibleWithStandardLibrary
+	if json.Unmarshal(payload, &event) != nil {
+		return
+	}
+	conn.eventsChan <- event
 }
 
 func (conn *Connection) onCall(payload []byte) {
@@ -157,4 +194,30 @@ func (conn *Connection) sendMsg(msg []byte) error {
 	ans := conn.raw.WriteMsg(msg)
 	conn.writeLock.Unlock()
 	return ans
+}
+
+func (conn *Connection) dealState() {
+	for state := range conn.statesChan {
+		i := strings.LastIndex(state.Name, "/")
+		if i == -1 {
+			continue
+		}
+		modelName := state.Name[:i]
+		stateName := state.Name[i+1:]
+
+		conn.stateHandler(modelName, stateName, state.Data)
+	}
+}
+
+func (conn *Connection) dealEvent() {
+	for event := range conn.eventsChan {
+		i := strings.LastIndex(event.Name, "/")
+		if i == -1 {
+			continue
+		}
+		modelName := event.Name[:i]
+		eventName := event.Name[i+1:]
+
+		conn.eventHandler(modelName, eventName, event.Args)
+	}
 }
