d3eb55b4a591246e538e58fa370d17b15dd54b94
hulele <hulelebuaa@163.com>
2023-12-13 16:12:42 +0800

    完成调用等待器的公有接口注释

--- model/respWaiter.go
+++ model/respWaiter.go
@@ -7,6 +7,7 @@ import (
 	"time"
 )
 
+// RespWaiter 为调用响应等待器, 用于等待调用请求报文的响应报文.
 type RespWaiter struct {
 	gotOnce sync.Once       // 保证 got 只关闭一次
 	got     chan struct{}   // 收到响应信号
@@ -22,11 +23,14 @@ func (w *RespWaiter) wake(resp message.RawResp, err error) {
 	})
 }
 
+// Wait 阻塞式地等待调用响应报文,直到收到调用响应报文或者连接关闭,返回响应报文的返回值和错误信息.
 func (w *RespWaiter) Wait() (message.RawResp, error) {
 	<-w.got
 	return w.resp, w.err
 }
 
+// WaitFor 阻塞式地等待调用响应报文,直到收到调用响应报文、等待时间超过timeout或者连接关闭,
+// 返回响应报文的返回值和错误信息.
 func (w *RespWaiter) WaitFor(timeout time.Duration) (message.RawResp, error) {
 	select {
 	case <-time.After(timeout):
